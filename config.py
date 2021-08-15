5d371b7b1cda08d49a1bab7e66dbe78b7456c0a5
dinesh-0602 <81961690+dinesh-0602@users.noreply.github.com>
2021-08-15 07:18:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,6 +22,7 @@ class Messages():
         "**Examples**\n```/inkick within_month long_time_ago``` - __To kick users who are offline for more than 6-7 days.__\n\n```/inkick long_time_ago``` - __To kick members who are offline for more than a month and Deleted Accounts.__\n\n```/dkick``` - __To kick deleted accounts.__",
         
         "**Developer - @viperadnan**"
+        
         "**Creator - @dinesh_060298**"
         ]
 
