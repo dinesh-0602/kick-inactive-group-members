73d0fe0c4799a1e881889412271d6cea4a6bcdcc
DINESH <81961690+dinesh-0602@users.noreply.github.com>
2021-11-03 11:09:41 +0530

    Typo

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Messages():
         
         "**Examples**\n```/inkick within_month long_time_ago``` - __To kick users who are offline for more than 6-7 days.__\n\n```/inkick long_time_ago``` - __To kick members who are offline for more than a month and Deleted Accounts.__\n\n```/dkick``` - __To kick deleted accounts.__",
         
-        "**Developer** - @viperadnan"\n**Creator** - @dinesh_060298\n** Join **- @VaathiCloud",
+        "**Developer** - @viperadnan\n\n**Creator** - @dinesh_060298\n** Join **- @VaathiCloud",
         ]
 
       START_MSG = "**Hey [{}](tg://user?id={})**\n__I can kick inactive members from group.\nLearn more at /help__"
